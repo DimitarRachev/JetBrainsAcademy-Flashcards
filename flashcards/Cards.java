ce17ee84472a6923da8331eb7a065747d0f8618a
Dimitar Rachev <DesolateBG@gmail.com>
2022-04-20 14:37:56 +0300

    Stage 4/7 complete

--- flashcards/Cards.java
+++ flashcards/Cards.java
@@ -4,20 +4,20 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Cards {
-     Map<String, String> cards;
+    Map<String, String> cards;
+    Map<String, String> reverseCards;
 
     public Cards() {
+        reverseCards = new LinkedHashMap<>();
         cards = new LinkedHashMap<>();
     }
 
     public void addCard(String face, String back) {
-        cards.putIfAbsent(face, back);
+        cards.put(face, back);
+        reverseCards.put(back, face);
     }
-
 
     public String getBack(String face) {
         return cards.get(face);
     }
-
-
 }
--- flashcards/Main.java
+++ flashcards/Main.java
@@ -1,7 +1,6 @@
 package flashcards;
 
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,18 +12,33 @@ public class Main {
         for (int i = 1; i <= n; i++) {
             System.out.println("Card #" + i + ":");
             String face = scanner.nextLine();
+            while (isUniq(face, cards.cards)) {
+                System.out.println("The term \"" + face + "\" already exists. Try again:");
+                face = scanner.nextLine();
+            }
             System.out.println("The definition for card #" + i + ":");
             String back = scanner.nextLine();
+            while (isUniq(back, cards.reverseCards)) {
+                System.out.println("The term \"" + back + "\" already exists. Try again:");
+                back = scanner.nextLine();
+            }
             cards.addCard(face, back);
 
         }
         for (var entry : cards.cards.entrySet()) {
             System.out.println("Print the definition of \"" + entry.getKey() + "\":");
             String answer = scanner.nextLine();
-            String output = answer.equals(entry.getValue()) ? "Correct!" : "Wrong. The right answer is \"" + entry.getValue() + "\".";
-            System.out.println(output);
+            if (answer.equals(entry.getValue())) {
+                System.out.println("Correct!");
+            } else if (cards.reverseCards.containsKey(answer)) {
+                System.out.println("Wrong. The right answer is \"" + entry.getValue() + "\", but your definition is correct for \"" + cards.reverseCards.get(answer) + "\".");
+            } else {
+                System.out.println("Wrong. The right answer is \"" + entry.getValue() + "\".");
+            }
         }
+    }
 
-
+    private static boolean isUniq(String str, Map<String, String> map) {
+        return map.containsKey(str);
     }
 }
